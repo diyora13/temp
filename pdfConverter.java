ebba2911458eba8786968d3b225c869ef1bab9b8
Manan Diyora <60748491+diyora13@users.noreply.github.com>
2023-09-15 15:06:12 +0530

    Update pdfConverter.java

--- pdfConverter.java
+++ pdfConverter.java
@@ -8,15 +8,11 @@ public void converAndDownload(String WebLink) {
     String Converted = "Yes";
     
     try {
-        Pdfcrowd.View webView = new Pdfcrowd.View(WebLink);
-
         Pdfcrowd.HtmlToPdfClient client = new Pdfcrowd.HtmlToPdfClient("user", "apikey");
-
         String outputFilename = WebLink.substring(WebLink.lastIndexOf('/') + 1) + ".pdf";
-
         client.convertUrlToFile(WebLink, outputFilename);
     }
-    catch(Pdfcrowd.Error e) {
+    catch(Pdfcrowd.Error e){
         System.err.println("Pdfcrowd Error: " + e.getMessage());
     }
     catch(IOException e) {
