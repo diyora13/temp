e39f1cb8bf12a3d206110a52788207c6ce0f98fd
Manan Diyora <60748491+diyora13@users.noreply.github.com>
2023-09-15 14:58:00 +0530

    Create pdfConverter.java

---
+++ pdfConverter.java
@@ -0,0 +1,25 @@
+import com.pdfcrowd.HtmlToPdfClient;
+import com.pdfcrowd.View;
+import com.pdfcrowd.Error;
+
+
+public void converAndDownload(String WebLink) {
+    
+    String Converted = "Yes";
+    
+    try {
+        Pdfcrowd.View webView = new Pdfcrowd.View(WebLink);
+
+        Pdfcrowd.HtmlToPdfClient client = new Pdfcrowd.HtmlToPdfClient("user", "apikey");
+
+        String outputFilename = WebLink.substring(WebLink.lastIndexOf('/') + 1) + ".pdf";
+
+        client.convertUrlToFile(WebLink, outputFilename);
+    }
+    catch(Pdfcrowd.Error e) {
+        System.err.println("Pdfcrowd Error: " + e.getMessage());
+    }
+    catch(IOException e) {
+        System.err.println("IO Error: " + e.getMessage());
+    }
+}
